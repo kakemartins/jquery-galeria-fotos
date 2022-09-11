f119d7b4bde89b0014d1744dae79c742c68673b4
Jackson Martins <jacksonhmartins@gmail.com>
2022-09-11 14:12:13 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+    
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoNovaImagem = $('#endereco-nova-imagem').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereço-nova-imagem').val('');
+    })
+})
+
